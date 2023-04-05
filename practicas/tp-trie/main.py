698a47dacb2d36ab1071f817c10197e32782219b
Victor Ramirez <victorriver6@hotmail.com>
2023-04-05 12:06:36 -0300

    "Version funcional insert"

--- practicas/tp-trie/main.py
+++ practicas/tp-trie/main.py
@@ -1,7 +1,12 @@
 import trie as t
 
 T = t.Trie()
-t.insert(T, "ABCD")
-t.insert(T, "JK")
-
+#Inserto 3 palabras normales
+t.insert(T, "HOLA")
+t.insert(T, "CHAU")
+t.insert(T, "ADIOS")
+#Inserto una palabra que tiene la misma rama que "HOLA"
+t.insert(T, "HOLANDA")
+#Inserto una palabra que provoca una bifurcacion
+t.insert(T,"CAOS")
 
--- practicas/tp-trie/trie.py
+++ practicas/tp-trie/trie.py
@@ -25,11 +25,11 @@ def insert(T, element):
         new_node = False
 
         if children != [] and children != None: #Si la lista no esta vacia busco el elemento
-            new_node = element[i] in children #Corregir esta parte para q devuelva la pos de un elemento
+            new_node = search_list(element[i] , children)  #Devuelve true o false
         else:
             current.children = children #Sino, dejo la lista vacia
         
-        if new_node == False: #Si no se encontro el elemento en la lista children
+        if new_node == False: #Si no se encontro el elemento en la lista children entonces:
             new_node = TrieNode()
             new_node.parent = current
             new_node.key = element[i]
@@ -39,11 +39,30 @@ def insert(T, element):
                 children = []
                 current.children = children
                 children.append(new_node)
+        else:                       #Si el elemento ya esta en la lista entonces:
+            index = search_pos(element[i] , children)
+            new_node = children[index]
+
+        if len(element)-1 == i:
+            new_node.isEndOfWord = True
 
         current = new_node
 
     return T
 
 
+def search_list(element , lista):
 
+    for i in range(len(lista)):
+        if element == lista[i].key:
+            return True
+        
+    return False
 
+def search_pos(element , lista):
+
+    for i in range(len(lista)):
+        if element == lista[i].key:
+            return i
+        
+    return None
