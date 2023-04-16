8b964e93aebd65a8225b7cbb365d7735823365ac
Victor Ramirez <victorriver6@hotmail.com>
2023-04-16 18:09:58 -0300

    New version of insert,search and delete

--- practicas/tp-hashtable/code/dictionary.py
+++ practicas/tp-hashtable/code/dictionary.py
@@ -1,28 +1,60 @@
-class Dictionaty:
-    value = None
-    key = None
+class DictionaryNone:
+    def __init__(self, key= None, value = None):
+        self.key = key
+        self.value = value
 
-def func_hash(D , key):
-    return key % len(D)
+class Dictionary:
 
-def insert(D , key , value):
-    
-    new_key = func_hash(D , key)
+    def __init__(self , hash_function = None , longitud = 10):
+        self.D = [None] * longitud
 
-    if D[new_key] == None:
-        new_node = Dictionaty()
-        new_node.value = value
-        new_node.key = key
-        D[new_key] = []
-        D[new_key].append(new_node)
-    else:
-        new_node = Dictionaty()
-        new_node.value = value
-        new_node.key = key
-        if search_list(value , D[new_key]) == False:
+        if hash_function != None:
+            self.hash_function = hash_function
+        else:
+            self.hash_function = lambda x: x % longitud
+
+    def insert(self , D , key , value):
+
+        new_key = self.hash_function(key)
+
+        if D[new_key] == None:
+            new_node = DictionaryNone(key , value)
+            D[new_key] = []
             D[new_key].append(new_node)
+        else:
+            new_node = DictionaryNone(key , value)
+            if search_list(value , D[new_key]) == False:
+                D[new_key].append(new_node)
+        return D
 
-    return D
+    def search(self , D , key):
+
+        new_key = self.hash_function(key)
+        if D[new_key] == None:
+            return None
+        else:
+            list = D[new_key]
+            pos = search_key(key , list)
+            if pos == None:
+                return None
+            else:
+                return list[pos].value
+
+    def delete(self , D , key):
+
+        new_key = self.hash_function(key)
+
+        if D[new_key] == None:
+            return D
+        else:
+            lista = D[new_key]
+            pos = search_key(key , lista)
+            if pos == None:
+                return D
+            else:
+                lista.pop(pos)
+                return D
+
 
 
 def search_list(element , lista):
@@ -30,7 +62,7 @@ def search_list(element , lista):
     for i in range(len(lista)):
         if lista[i] != None:
             if element == lista[i].value:
-                return True       
+                return True
     return False
 
 def search_key(key , lista):
@@ -38,36 +70,5 @@ def search_key(key , lista):
     for i in range(len(lista)):
         if lista[i] != None:
             if key == lista[i].key:
-                return i       
+                return i
     return None
-
-def search(D , key):
-    
-    new_key = func_hash(D , key)
-    if D[new_key] == None:
-        return None
-    else:
-        list = D[new_key]
-        pos = search_key(key , list)
-        if pos == None:
-            return None
-        else:
-            return list[pos].value
-        
-def delete(D , key):
-
-    new_key = func_hash(D , key)
-
-    if D[new_key] == None:
-        return D
-    else:
-        lista = D[new_key]
-        pos = search_key(key , lista)
-        if pos == None:
-            return D
-        else:
-            lista.pop(pos)
-            return D
-
-
-
--- practicas/tp-hashtable/code/main.py
+++ practicas/tp-hashtable/code/main.py
@@ -1,44 +1,48 @@
 import dictionary as d
-#D = [5, 28, 19, 15, 20, 33, 12, 17, 10]
-N = 5
-D = [None] * N
-print(d.func_hash(D , 9))
+import parte2 as p2
+
+dictionary = d.Dictionary(None , 8)
 #--------Insert--------
 #Si el diccionario esta vacio:
-d.insert(D , 0 , "H0")
-d.insert(D , 1 , "H1")
-d.insert(D , 2 , "H2")
-d.insert(D , 3 , "H3")
-d.insert(D , 4 , "H4")
-d.insert(D , 5 , "H5")
+dictionary.insert(dictionary.D , 0 , "H0")
+dictionary.insert(dictionary.D , 1 , "H1")
+dictionary.insert(dictionary.D , 2 , "H2")
+dictionary.insert(dictionary.D , 3 , "H3")
+dictionary.insert(dictionary.D , 4 , "H4")
+dictionary.insert(dictionary.D , 5 , "H5")
+dictionary.insert(dictionary.D , 6 , "H6")
+dictionary.insert(dictionary.D , 7 , "H7")
 #Si no esta vacio:
-d.insert(D , 10 , "H10")
-d.insert(D , 9 , "H9")
+dictionary.insert(dictionary.D , 10 , "H10")
+dictionary.insert(dictionary.D , 9 , "H9")
 #Si en la key ya hay un valor que ya estaba entonces:
-d.insert(D , 20 , "H0")
+dictionary.insert(dictionary.D , 8 , "H0")
+
 
 print("--------Search--------")
 #--------Search--------
-print(d.search(D , 0))
-print(d.search(D , 3))
-print(d.search(D , 5))
-print(d.search(D , 10))
-print(d.search(D , 100))
+print(dictionary.search(dictionary.D , 0))
+print(dictionary.search(dictionary.D , 3))
+print(dictionary.search(dictionary.D , 5))
+print(dictionary.search(dictionary.D , 10))
+print(dictionary.search(dictionary.D , 100))
 
 print("--------Delete--------")
 #--------Delete--------
-d.delete(D , 0)
-d.delete(D , 100)
-d.delete(D , 3) #Solo hay un elemento y lo elemino
+dictionary.delete(dictionary.D , 0)
+dictionary.delete(dictionary.D , 100)
+dictionary.delete(dictionary.D , 3) #Solo hay un elemento y lo elemino
+
 #Ahora quiero agregar algo en ese lugar
-d.insert(D , 3 , "H3")
+dictionary.insert(dictionary.D , 3 , "H3")
+dictionary.insert(dictionary.D , 0 , "H1")
+
+#--------PARTE 2--------
 print("--------Ejercicio 4--------")
-print(d.is_permutation("HOLA" , "ALHO"))
-print(d.is_permutation("HOLA" , "ALHODA"))
+print(p2.is_permutation("HOLA" , "ALHO"))
+print(p2.is_permutation("HOLA" , "ALHODA"))
 
 print("--------Ejercicio 5--------")
-print(d.isSet("ADIOS"))
-print(d.isSet("HHOLA"))
+print(p2.isSet("ADIOS"))
+print(p2.isSet("HHOLA"))
 print("--------FIN--------")
-print(ord(1))
-print(ord(2))
